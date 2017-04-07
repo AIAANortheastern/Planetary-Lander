69212a2bedcc44b3e4b4842203a6f0595f15f466
Jacob Tatz <jacob@tatz.io>
2017-04-06 21:20:35 -0400

    LDRS test launch ready code

--- Lander/Lander.ino
+++ Lander/Lander.ino
@@ -24,9 +24,9 @@ template<class T> String &operator<<(String &lhs, T &rhs) {
 template<> String &operator<<(String &lhs, TSL2591 &rhs) {
     float lux;
     if (rhs.getLux(&lux)) {
-        lhs << "Error with TSL2591 sensor" << "\n";
+        lhs << "Error with TSL2591 sensor";
     } else {
-        lhs << "Lux: " << lux << "\n";
+        lhs << "{Lux:" << lux << "}";
     }
     return lhs;
 }
@@ -34,11 +34,12 @@ template<> String &operator<<(String &lhs, TSL2591 &rhs) {
 template<> String &operator<<(String &lhs, BME280 &rhs) {
     double temp, pres, humd;
     if (rhs.read_processed(&temp, &pres, &humd)) {
-        lhs << "Error with BME280 sensor" << "\n";
+        lhs << "Error with BME280 sensor";
     } else {
-        lhs << "Temperature: " << temp << "\n";
-        lhs << "Pressure: " << pres << "\n";
-        lhs << "Humidity: " << humd << "\n";
+        lhs << "{";
+        lhs << "Temperature:" << temp << ",";
+        lhs << "Pressure:" << pres << ",";
+        lhs << "Humidity:" << humd << "}";
     }
     return lhs;
 }
@@ -47,56 +48,65 @@ template<> String &operator<<(String &lhs, MPU9250 &rhs) {
     int16_t gyro[3], acc[3], magno[3];
     //float orientation[] = {0.0, 0.0, 0.0, 0.0};
     if (rhs.readGyrometerData(gyro) || rhs.readAccelometerData(acc) || rhs.readMagneoometerData(magno)){// || rhs.getQuaternion(orientation, gyro, acc, magno)) {
-        lhs << "Error with MPU9250 sensor" << "\n";
+        lhs << "Error with MPU9250 sensor";
     } else {
-        lhs << "Gyrometer: " << gyro[0] << "," << gyro[1] << "," << gyro[2] << "\n";
-        lhs << "Accelerometer: " << acc[0] << "," << acc[1] << "," << acc[2] << "\n";
-        lhs << "Magnetometer: " << magno[0] << "," << magno[1] << "," << magno[2] << "\n";
-        //lhs << "Orientation: " << orientation[0] << "," << orientation[1] << "," << orientation[2] << "," << orientation[3] << "\n";
+        lhs << "{";
+        lhs << "Gyrometer:[" << gyro[0] << "," << gyro[1] << "," << gyro[2] << "],";
+        lhs << "Accelerometer:[" << acc[0] << "," << acc[1] << "," << acc[2] << "],";
+        lhs << "Magnetometer:[" << magno[0] << "," << magno[1] << "," << magno[2] << "]}";
+        //lhs << "Orientation:[" << orientation[0] << "," << orientation[1] << "," << orientation[2] << "," << orientation[3] << "]}";
     }
     return lhs;
 }
 
-
-void setup() {
-    Serial.begin(9600);
-    while(!Serial){}
-    Serial.println("Connected");
+void setup(){
+    //On-Board led
+    pinMode(13, OUTPUT);
+    //SD Card
+    if(!SD.begin(BUILTIN_SDCARD)){
+        //turn on led if failed
+        digitalWrite(13, HIGH);
+        return;
+    }
+    File dataFile = SD.open("log.txt", FILE_WRITE);
     // Pam7Q
+    /*
     Serial2.begin(9600);
     while(!Serial2) {}
+     */
     //I2c
     Wire.begin();
     //TSL2591
+    /*
     if(tsl.start(TSL2591_GAIN_1X, TSL2591_INTEGRATION_TIME_100MS)){
-      Serial.println("Couldn't connect to TSL2591 sensor");
+        dataFile.println("Couldn't connect to TSL2591 sensor");
     }
+     */
     //BME280
     if (bme.start()) {
-      Serial.println("Couldn't connect to BME280 sensor");
+        dataFile.println("Couldn't connect to BME280 sensor");
     }
     else {
         delay(300);
-        while(bme.isReadingCalibration()) delay(100);
+        int i = 0;
+        while(bme.isReadingCalibration() && i++ < 100) delay(100);
         bme.set(BME280_16x_OVERSAMPLING, BME280_16x_OVERSAMPLING, BME280_16x_OVERSAMPLING);
     }
     //MPU9250
     if(mpu.start()){
-        Serial.println("Couldn't connect to MPU9250 sensor");
+        dataFile.println("Couldn't connect to MPU9250 sensor");
     }
-    //SD Card
-    /*
-    if(!SD.begin(BUILTIN_SDCARD)){
-        Serial.println("Couldn't connect to SD card.");
-    }
-    */
+    dataFile.close();
 }
 
 void loop() {
-    String data;
-    data << "{\nTSL:" << tsl << ",\nBME:" << bme << "\nMPU:" << mpu << "\n}";
-    Serial.print(data << mpu);
+    File dataFile = SD.open("log.txt", FILE_WRITE);
+    String jsonData;
+    // jsonData << "{TSL:" << tsl << ",BME:" << bme << ",MPU: " << mpu << "}";
+    jsonData << "{BME:" << bme << ",MPU: " << mpu << "}";
+    dataFile.println(jsonData);
     //Pam7Q
+    /*
     bool newdata = false;
     unsigned long start = millis();
     while (millis() - start < 5000) {
@@ -107,14 +117,9 @@ void loop() {
     if (newdata) {
         Serial.print("Pam7Q: ");
         gpsdump(gps);
-    }
-    /*
-    File dataFile = SD.open("DATA.TXT", FILE_WRITE);
-    dataFile.print("Test: ");
-    dataFile.println(125);
+    }*/
     dataFile.close();
-    */
-    delay(500);
+    delay(50);
 }
 
 
