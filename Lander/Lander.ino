48f156e699913f8b04859f029410304993f3ca91
ZachMarcus <zachary.j.marcus@gmail.com>
2017-04-19 19:01:28 -0400

    added camera setup, need to add everything to loop.

--- Lander/Lander.ino
+++ Lander/Lander.ino
@@ -7,6 +7,7 @@
 
 #include <Wire.h>
 #include <SD.h>
+#include <SPI.h>
 
 #include "src/TSL2591.h"
 #include "src/BME280.h"
@@ -14,11 +15,25 @@
 #include "src/TinyGPS.h"
 #include "src/ArduCAM.h"
 #include "src/fastlz.h"
+#include "src/memorysaver.h"
 
 TSL2591 tsl = TSL2591();
 BME280 bme = BME280(0x77);//0x76 if SDI grounded, or 0x77 if SDI is attached to logic level
 MPU9250 mpu = MPU9250(0x68);// 0x68 if AD0 is grounded, 0x69 if AD0 is at logic level
 TinyGPS gps;
+
+// Stuff for camera to function appropriately
+#define SD_CS 9
+const int SPI_CS = 10; // used to be 7, redefined it to be 15 for current wiring.
+#if defined (OV2640_MINI_2MP)
+  ArduCAM myCAM(OV2640, SPI_CS);
+#else
+  ArduCAM myCAM(OV5642, SPI_CS);
+#endif
+
+
+
+
 
 template<class T> String &operator<<(String &lhs, T &rhs) {
     return lhs += rhs;
@@ -88,7 +103,7 @@ void setup(){
         Serial.println("Couldn't connect to TSL2591 sensor");
     }
 
-  Serial.println("Pre BME");
+    Serial.println("Pre BME");
     
     //BME280
     if (bme.start()) {
@@ -105,7 +120,69 @@ void setup(){
         Serial.println("Couldn't connect to MPU9250 sensor");
     }
 
-    Serial.print("Setup");
+    // ArduCAM
+    uint8_t vid, pid;
+    uint8_t temp;
+    pinMode(SPI_CS,OUTPUT);
+    SPI.begin();
+    int errorCount = 0;
+    while(1) {
+      // Check if the ArduCAM SPI bus is OK
+      myCAM.write_reg(ARDUCHIP_TEST1, 0x55);
+      temp = myCAM.read_reg(ARDUCHIP_TEST1);
+
+      if (temp != 0x55) {
+        Serial.println(F("SPI interface Error!"));
+        errorCount++;
+        delay(1000);
+      } else {
+        Serial.println(F("SPI interface OK."));
+        break;
+      }
+      if (errorCount > 10) {
+        break;
+      }
+    }
+
+    #if defined (OV2640_MINI_2MP)
+      while(1){
+        //Check if the camera module type is OV2640
+        myCAM.wrSensorReg8_8(0xff, 0x01);
+        myCAM.rdSensorReg8_8(OV2640_CHIPID_HIGH, &vid);
+        myCAM.rdSensorReg8_8(OV2640_CHIPID_LOW, &pid);
+        if ((vid != 0x26 ) && (( pid != 0x41 ) || ( pid != 0x42 ))){
+          Serial.println(F("Can't find OV2640 module!"));
+          delay(1000);continue;
+        }
+        else{
+          Serial.println(F("OV2640 detected."));break;
+        } 
+      }
+    #else
+      while(1){
+        //Check if the camera module type is OV5642
+        myCAM.wrSensorReg16_8(0xff, 0x01);
+        myCAM.rdSensorReg16_8(OV5642_CHIPID_HIGH, &vid);
+        myCAM.rdSensorReg16_8(OV5642_CHIPID_LOW, &pid);
+        if((vid != 0x56) || (pid != 0x42)){
+          Serial.println(F("Can't find OV5642 module!"));
+          delay(1000);continue;
+        }
+        else{
+          Serial.println(F("OV5642 detected."));break;
+        } 
+      }
+    #endif
+    myCAM.set_format(JPEG);
+    myCAM.InitCAM();
+    #if defined (OV2640_MINI_2MP)
+      myCAM.OV2640_set_JPEG_size(OV2640_640x480);
+    #else
+      myCAM.write_reg(ARDUCHIP_TIM, VSYNC_LEVEL_MASK);   //VSYNC is active HIGH
+      myCAM.OV5642_set_JPEG_size(OV5642_320x240);
+    #endif
+
+    Serial.println("Setup Completed");
 }
 
 void loop() {
