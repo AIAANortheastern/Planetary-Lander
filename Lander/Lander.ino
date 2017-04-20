6c1e072db431f2cd397c4d6d3ac6773e347260a3
Jacob Tatz <jacob@tatz.io>
2017-04-20 10:30:08 -0400

    Working MPU9250 code

--- Lander/Lander.ino
+++ Lander/Lander.ino
@@ -8,6 +8,7 @@
 #include <Wire.h>
 #include <SD.h>
 
+#include "src/i2c.h"
 #include "src/TSL2591.h"
 #include "src/BME280.h"
 #include "src/MPU9250.h"
@@ -17,7 +18,7 @@
 
 TSL2591 tsl = TSL2591();
 BME280 bme = BME280(0x77);//0x76 if SDI grounded, or 0x77 if SDI is attached to logic level
-MPU9250 mpu = MPU9250(0x68);// 0x68 if AD0 is grounded, 0x69 if AD0 is at logic level
+MPU9250 mpu;
 TinyGPS gps;
 
 template<class T> String &operator<<(String &lhs, T &rhs) {
@@ -48,16 +49,41 @@ template<> String &operator<<(String &lhs, BME280 &rhs) {
 }
 
 template<> String &operator<<(String &lhs, MPU9250 &rhs) {
-    int16_t gyro[3], acc[3], magno[3];
-    //float orientation[] = {0.0, 0.0, 0.0, 0.0};
-    if (rhs.readGyrometerData(gyro) || rhs.readAccelometerData(acc) || rhs.readMagneoometerData(magno)){// || rhs.getQuaternion(orientation, gyro, acc, magno)) {
-        lhs << "Error with MPU9250 sensor";
-    } else {
+    if (rhs.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01) {
+        rhs.readAccelData(rhs.accelCount);
+        rhs.getAres();
+        rhs.readGyroData(rhs.gyroCount);
+        rhs.getGres();
+        rhs.readMagData(rhs.magCount);
+        rhs.getMres();
+
+        rhs.ax = (float) rhs.accelCount[0] * rhs.aRes;
+        rhs.ay = (float) rhs.accelCount[1] * rhs.aRes;
+        rhs.az = (float) rhs.accelCount[2] * rhs.aRes;
+
+        rhs.gx = (float) rhs.gyroCount[0] * rhs.gRes;
+        rhs.gy = (float) rhs.gyroCount[1] * rhs.gRes;
+        rhs.gz = (float) rhs.gyroCount[2] * rhs.gRes;
+
+        rhs.mx = (float) rhs.magCount[0] * rhs.mRes * rhs.magCalibration[0] - rhs.magbias[0];
+        rhs.my = (float) rhs.magCount[1] * rhs.mRes * rhs.magCalibration[1] - rhs.magbias[1];
+        rhs.mz = (float) rhs.magCount[2] * rhs.mRes * rhs.magCalibration[2] - rhs.magbias[2];
+
+        rhs.updateTime();
+        MahonyQuaternionUpdate(rhs.ax, rhs.ay, rhs.az, rhs.gx*DEG_TO_RAD, rhs.gy*DEG_TO_RAD, rhs.gz*DEG_TO_RAD, rhs.my, rhs.mx, rhs.mz, rhs.deltat);
+        rhs.delt_t = millis() - rhs.count;
+        float *orientation = getQ();
+        rhs.count = millis();
+        rhs.sumCount = 0;
+        rhs.sum = 0;
+
         lhs << "{";
-        lhs << "Gyrometer:[" << gyro[0] << "," << gyro[1] << "," << gyro[2] << "],";
-        lhs << "Accelerometer:[" << acc[0] << "," << acc[1] << "," << acc[2] << "],";
-        lhs << "Magnetometer:[" << magno[0] << "," << magno[1] << "," << magno[2] << "]}";
-        //lhs << "Orientation:[" << orientation[0] << "," << orientation[1] << "," << orientation[2] << "," << orientation[3] << "]}";
+        lhs << "Gyrometer:[" << rhs.gx << "," << rhs.gy << "," << rhs.gz << "],";
+        lhs << "Accelerometer:[" << 1000*rhs.ax << "," << 1000*rhs.ay << "," << 1000*rhs.az << "],";
+        lhs << "Magnetometer:[" << rhs.mx << "," << rhs.my << "," << rhs.mz << "],";
+        lhs << "Orientation:[" << orientation[0] << "," << orientation[1] << "," << orientation[2] << "," << orientation[3] << "]}";
+    } else {
+        lhs << "Error with MPU9250 sensor";
     }
     return lhs;
 }
@@ -101,8 +127,16 @@ void setup(){
         bme.set(BME280_16x_OVERSAMPLING, BME280_16x_OVERSAMPLING, BME280_16x_OVERSAMPLING);
     }
     //MPU9250
-    if(mpu.start()){
+    if(read8(MPU9250_ADDRESS, WHO_AM_I_MPU9250) != 0x71){
         Serial.println("Couldn't connect to MPU9250 sensor");
+    } else {
+        mpu.calibrateMPU9250(mpu.gyroBias, mpu.accelBias);
+        mpu.initMPU9250();
+        if(read8(AK8963_ADDRESS, WHO_AM_I_AK8963) != 0x48){
+            Serial.println("Couldn't connect to AK8963 sensor");
+        } else {
+            mpu.initAK8963(mpu.magCalibration);
+        }
     }
 
     Serial.print("Setup");
